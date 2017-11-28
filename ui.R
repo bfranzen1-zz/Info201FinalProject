9855008f4732d1b5b0a9d83801235e037fbe294a
Blake <bef6823@gmail.com>
2017-11-27 16:54:30 -0800

    testing tabs

--- ui.R
+++ ui.R
@@ -3,6 +3,13 @@ ui <- fluidPage(
   titlePanel('Video Game Application'),
   sidebarLayout(
     sidebarPanel(),
-    mainPanel()
+    mainPanel(
+      tabsetPanel(
+        tabPanel('Tab1', 'This is Tab 1'),
+        tabPanel('Tab2', 'This is Tab 2'),
+        tabPanel('Tab3', 'This is Tab 3')
+      )
+      
+    )
   )
 )
