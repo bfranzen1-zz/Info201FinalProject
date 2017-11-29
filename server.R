44e965fd4472a797205c52b28d23a1c848226d0a
Blake <bef6823@gmail.com>
2017-11-29 14:05:54 -0800

    ss

--- server.R
+++ server.R
@@ -1,16 +1,20 @@
 #get data using key 
-source('keys.R')
 library(httr)
 library(jsonlite)
+source('searchGame.R')
 #add endpoint you want before running this code i.e.io/games/1942
 #this gives info on Witcher 3 
 url <- "https://api-2445582011268.apicast.io/"
-response <- GET(url, add_headers(.headers = c("user-key" = key, "Accept" = "application/json")))
-body <- content(response, "text")
-data <- fromJSON(body)
+#response <- GET(url, add_headers(.headers = c("user-key" = key, "Accept" = "application/json")))
+#body <- content(response, "text")
+#data <- fromJSON(body)
 server <- function(input, output) {
-  
-  
+  names <- data.frame()
+  output$game <- renderPrint({ 
+    gameList <- games(input$game)
+    names <- nameList(gameList)
+    return(gsub('\\.', ' ', colnames(names)))
+  })
   
   
   
